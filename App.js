bb6ba4fbc61643e7af45dbe771eedd7ef805ea3d
Zack Wallace <zwallace0790@gmail.com>
2021-12-05 01:05:09 -0800

    Added some styling with UIKit, getting the base layout down for the document editor

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Document from './components/Document';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Document />
+    </div>
+  );
+}
+
+export default App;
